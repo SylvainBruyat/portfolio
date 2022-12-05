737049be5aa7472b1a51c73c5f5666423f0bf9fc
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-12-05 16:22:20 +0100

    Update of style and picture source for component Techno

--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -1,14 +1,18 @@
 export default function Techno({ name, logo }) {
   return (
-    <li>
-      {/* Remplacer les images par des Font Awesomes ? Permettrait un meilleur rendu au niveau des couleurs
+    <>
+      {logo !== '' && (
+        <li className="techno-card">
+          {/* Remplacer les images par des Font Awesomes ? Permettrait un meilleur rendu au niveau des couleurs
     mais moins facile d'identifier les technos à cause des couleurs */}
-      <img
-        src={`/assets/icons/${logo}`}
-        alt={name}
-        width="40px"
-        height="40px"
-      />
-    </li>
+          <img
+            src={require(`../assets/icons/${logo}`)}
+            alt={name}
+            width="40px"
+            height="40px"
+          />
+        </li>
+      )}
+    </>
   );
 }
