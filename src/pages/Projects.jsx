53ae99e6044956ed713a412fb428aa9c2606d250
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-12-14 17:42:27 +0100

    Addition of text on page Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,6 +6,12 @@ export default function Projects() {
   return (
     <main>
       <h2>Portfolio</h2>
+      <p>Voici mes derniers projets réalisés.</p>
+      <p>
+        Certains seront un peu long à s'afficher au départ. Ce ne sont pas des
+        problèmes de performance. Il faut juste un peu de temps pour que le
+        service d'hébergement gratuit se réveille.{' '}
+      </p>
       <section className="projects-wrapper">
         {projects.map((project) => (
           <ProjectCard
