5362582836787868c6c513277acba5403b34cfd7
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-12-16 18:39:41 +0100

    Correction of environment variable names

--- src/services/contact-form-functions.jsx
+++ src/services/contact-form-functions.jsx
@@ -2,10 +2,10 @@ import emailjs from '@emailjs/browser';
 
 export async function sendEmail(form) {
   const response = await emailjs.sendForm(
-    process.env.EMAIL_SERVICE,
-    process.env.EMAIL_TEMPLATE,
+    process.env.REACT_APP_EMAIL_SERVICE,
+    process.env.REACT_APP_EMAIL_TEMPLATE,
     form,
-    process.env.EMAIL_PUBLIC_KEY
+    process.env.REACT_APP_EMAIL_PUBLIC_KEY
   );
   return response;
 }
