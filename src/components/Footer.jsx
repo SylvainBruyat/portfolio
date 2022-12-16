39c2470707faea607f4b6a28a9d6dd401bbf2863
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-12-16 17:12:17 +0100

    Addition of LinkedIn and Github links in component Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,34 @@
 /* A mettre dans le footer :
-- Liens vers mes réseaux et sites :
+Liens vers mes réseaux et sites :
   - LinkedIn
   - Github
   - ... (Codewars ou autre si pertinent)
-- Liens vers les différentes pages du site ? Redondant avec le menu ? Probablement à éviter avec le menu fixe en haut
 */
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p className="footer__text">Sylvain Bruyat © 2022</p>
+      <ul className="footer__links">
+        <li>
+          <a
+            href="https://www.linkedin.com/in/sylvain-bruyat/"
+            title="Mon profil LinkedIn"
+            className="footer__link"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/SylvainBruyat"
+            title="Mon profil Github"
+            className="footer__link"
+          >
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
