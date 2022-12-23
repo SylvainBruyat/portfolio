b12f086c67369a1c099f76190ee72f3a57809234
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-12-24 00:53:22 +0100

    Addition of a ScrollToTop function on location change

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,21 @@ import NotFound from './pages/NotFound';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './utils/ScrollToTop';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </ScrollToTop>
       <Footer />
     </>
   );
